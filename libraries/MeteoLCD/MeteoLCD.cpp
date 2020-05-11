6a3254d852f1ef8a079464ed11975a3a0e97f2db
Roman <dontsovroman@gmail.com>
2020-05-11 23:42:09 +0300

    METEO UPDATE bmp280

--- libraries/MeteoLCD/MeteoLCD.cpp
+++ libraries/MeteoLCD/MeteoLCD.cpp
@@ -1,9 +1,9 @@
 #include "MeteoLCD.h"
 
-MeteoLCD::MeteoLCD(DHT *_dht, UTFT *_display, iarduino_RTC *_rtc, GasSensor *_gas, int pixelPerChar)
+MeteoLCD::MeteoLCD(Adafruit_BMP280 *_bmp280, UTFT *_display, iarduino_RTC *_rtc, GasSensor *_gas, int pixelPerChar)
 {
   this->display = _display;
-  this->dht = _dht;
+  this->bmp280 = _bmp280;
   this->rtc = _rtc;
   this->pixelPerChar = pixelPerChar;
   this->gas = _gas;
@@ -19,20 +19,29 @@ String MeteoLCD::getValue(float val)
 void MeteoLCD::init()
 {
   delay(300);
-  dht->begin();
+  while (!bmp280->begin(BMP280_ADDRESS - 1)) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
+    delay(2000);
+  }
   rtc->begin();
   display->InitLCD(1);
   display->clrScr();
 }
 
-float MeteoLCD::getHumidity()
+float MeteoLCD::getPressure()
 {
-  return dht->readHumidity();
+  Serial.println(bmp280->readPressure());
+  Serial.println(bmp280->readPressure() / 133.322);
+  return bmp280->readPressure() / 133.322;
 }
 
 float MeteoLCD::getTemperature()
 {
-  return dht->readTemperature();
+  return bmp280->readTemperature();
+}
+
+float MeteoLCD::getAltitude(){
+  return bmp280->readAltitude();
 }
 
 String MeteoLCD::getAir()
@@ -57,11 +66,12 @@ void MeteoLCD::print()
   display->setColor(VGA_WHITE);
   display->setFont(BigFont);
   display->print(TEMPERATURE + getValue(getTemperature()) + C, LEFT, 0);
-  display->print(HUMIDITY + getValue(getHumidity()) + H, LEFT, pixelPerChar * 2);
+  display->print(PRESSURE + String(getPressure()) + P, LEFT, pixelPerChar * 2);
+  display->print(ALTITUDE + String(getAltitude()) + ALT, LEFT, pixelPerChar * 4);
   display->print(getAir(), LEFT, pixelPerChar * 16);
   display->setColor(VGA_WHITE);
 
   display->setFont(Grotesk32x64);
-  display->print(rtc->gettime("d.m.Y"), CENTER, pixelPerChar * 6);
-  display->print(rtc->gettime("H:i, D"), CENTER, pixelPerChar * 10);
+  display->print(rtc->gettime("H:i, D"), CENTER, pixelPerChar * 6);
+  display->print(rtc->gettime("d.m.Y"), CENTER, pixelPerChar * 10);
 }
--- libraries/MeteoLCD/MeteoLCD.h
+++ libraries/MeteoLCD/MeteoLCD.h
@@ -11,14 +11,17 @@
 #include "DHT.h"
 #include <Adafruit_Sensor.h>
 #include <GasSensor.h>
+#include <Adafruit_BMP280.h>
 
 extern uint8_t BigFont[];
 extern uint8_t Grotesk32x64[];
 
 const char TEMPERATURE[] = "Temperature: ";
 const char C[] = "C";
-const char HUMIDITY[] = "Humidity: ";
-const char H[] = "%";
+const char PRESSURE[] = "Pressure: ";
+const char P[] = "mmHg";
+const char ALTITUDE[] = "Altitude: ";
+const char ALT[] = "m";
 const char AIR[] = "Air is ";
 const char CLEAR[] = "clear";
 const char DIRTY[] = "dirty";
@@ -26,15 +29,17 @@ const char DIRTY[] = "dirty";
 class MeteoLCD : public MyLcd
 {
 public:
-  MeteoLCD(DHT *_dht, UTFT *_display, iarduino_RTC *_rtc, GasSensor *_gas, int pixelPerChar);
+  MeteoLCD(Adafruit_BMP280 *bmp280, UTFT *_display, iarduino_RTC *_rtc, GasSensor *_gas, int pixelPerChar);
   void init();
-  float getHumidity();
+  float getPressure();
   float getTemperature();
+  float getAltitude();
+  String getValue(float value);
   void print();
 
 protected:
   String getAir();
-  DHT *dht;
+  Adafruit_BMP280 *bmp280;
   UTFT *display;
   iarduino_RTC *rtc;
   GasSensor *gas;
