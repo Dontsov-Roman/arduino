82b6173ce165d2a823a57a0f889e85f4452c4dca
Roman <rdontsov@lohika.com>
2020-05-18 17:14:44 +0300

    Zixa bmp

---
+++ Zexa_bmp/Zexa_bmp.ino
@@ -0,0 +1,20 @@
+
+#include <LiquidCrystal.h>
+#include <Adafruit_BMP280.h>
+#include <SensorButton.h>
+
+SensorButton btn(8, true);
+
+Adafruit_BMP280 bmp280;
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
