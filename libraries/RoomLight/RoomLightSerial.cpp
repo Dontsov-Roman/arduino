ce55dc9fafacf19eda54189764bb2fda350fb43b
Dontsov Roman <dontsovroman@gmail.com>
2024-08-20 21:50:25 +0300

    Issue with static_cast from char to enum

--- libraries/RoomLight/RoomLightSerial.cpp
+++ libraries/RoomLight/RoomLightSerial.cpp
@@ -22,46 +22,37 @@ TransferStruct* RoomLightSerial::getTransferStruct() {
 
 void RoomLightSerial::clearTransferStruct() {
     this->transferStruct.value = "";
+    this->transferBuf = "";
 }
 
 void RoomLightSerial::read() {
     if(this->serial->available()) {
-        this->transferBuf = "";
-        while (this->serial->available() > 0) {         // ПОКА есть что то на вход    
-            this->transferBuf += (char)this->serial->read();        // забиваем строку принятыми данными
-            delay(2);                              // ЗАДЕРЖКА. Без неё работает некорректно!
+        this->clearTransferStruct();
+        while (this->serial->available() > 0) {  
+            this->transferBuf += (char)this->serial->read();
+            delay(2);
         }
-        Serial.println(this->transferBuf);
-        // this->receivedChar = String(this->serial->read());
-        // delay(2);
-        // if(this->receivedChar == this->startChar){
-        //     this->lastControlChar = this->receivedChar;
-        //     this->transferStruct.command = static_cast<RoomLightCommands>(this->serial->read());
-        //     delay(2);
-        //     this->receivedChar = String(this->serial->read());
-        //     delay(2);
-        //     Serial.println("Last chars:");
-        //     Serial.println(this->receivedChar);
-        //     Serial.println(this->lastControlChar);
-        //     if(this->receivedChar == this->eolChar){
-        //         this->lastControlChar = this->receivedChar;
-        //         this->transferStruct.value = "";
-        //         while (this->serial->available() > 0) {
-        //             this->receivedChar = this->serial->read();
-        //             if(this->receivedChar != this->termChar){
-        //                 this->transferStruct.value += (char)this->serial->read();
-        //                 delay(2);
-        //             }
-        //         }
-        //     }
-        // }
-        
-        // this->serial->readBytes((byte*)&this->transferStruct, this->serial->available());
-        // Serial.println("Last command:");
-        // Serial.println(this->transferStruct.command);
-        // Serial.println("Last string:");
-        // Serial.println(this->transferStruct.value);
-        // delay(10);
+        for(char &c : this->transferBuf) {
+            this->receivedChar = String(c);
+            if (this->receivedChar == this->startChar || this->receivedChar == this->eolChar || this->receivedChar == this->termChar) {
+                this->lastControlChar = String(c);
+                continue;
+            }
+            if (this->lastControlChar == this->startChar) {
+                this->transferStruct.command = static_cast<RoomLightCommands>(c);
+                Serial.println(this->receivedChar);
+                Serial.println(c);
+                Serial.println(this->transferStruct.command);
+            } else if(this->lastControlChar == this->eolChar) {
+                this->transferStruct.value += this->receivedChar;
+            } else if(this->lastControlChar == this->termChar) {
+                break;
+            }
+        }
+        Serial.println("Last command:");
+        Serial.println(this->transferStruct.command);
+        Serial.println("Last string:");
+        Serial.println(this->transferStruct.value);
     }
 }
 
@@ -71,15 +62,5 @@ void RoomLightSerial::write(RoomLightCommands command) {
 }
 
 void RoomLightSerial::write(RoomLightCommands command, String value) {
-    this->transferStruct.command = command;
-    // strcpy(this->transferStruct.address, address.c_str());
-    // this->transferStruct.value = this->stringToChar(value);
-    this->transferStruct.value = value;
-    // String str = "command:" + String(command) + "value:" + value;
-    // this->serial->write("test", sizeof("test"));
-
-    // this->serial->write((byte*)&transferStruct, sizeof(transferStruct));
-    this->serial->print(this->startChar + String(this->transferStruct.command) + this->eolChar + String(this->transferStruct.value) + this->termChar);
-    // this->serial->print("command:" + String(command) + "value:" + value);
-    // this->serial->print("command:");
+    this->serial->print(this->startChar + static_cast<String>(command) + this->eolChar + String(value) + this->termChar);
 }
