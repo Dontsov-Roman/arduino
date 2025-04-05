9412219ff973909579d76601ba1c7464a3a14f84
Dontsov Roman <dontsovroman@gmail.com>
2025-04-06 01:20:37 +0300

    Display and Http doesnt work with esp32

--- GpsCarHome/GpsCarHome.ino
+++ GpsCarHome/GpsCarHome.ino
@@ -1,12 +1,12 @@
-#include <GpsCar.h>
+// #include <GpsCar.h>
 #include <SoftwareSerial.h>
 #include <GpsReader.h>
-#include <SimpleOled.h>
+// #include <SimpleOled.h>
 
-#include <SPI.h>
-#include <Wire.h>
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
+// #include <SPI.h>
+// #include <Wire.h>
+// #include <Adafruit_GFX.h>
+// #include <Adafruit_SSD1306.h>
 
 #include <WifiClientEsp32.h>
 #include <HttpClientEsp32.h>
@@ -15,12 +15,12 @@
 #define SCREEN_HEIGHT 32    // OLED display height, in pixels
 #define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-SimpleOled simpleOled(&display);
+// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+// SimpleOled simpleOled(&display);
 
 #ifndef STASSID
-#define STASSID "UFI-495947"
-#define STAPSK "12345678"
+// #define STASSID "UFI-495947"
+// #define STAPSK "12345678"
 // #define STASSID "car_wifi"
 #define STASSID "56"
 #define STAPSK "dontsovaAlya"
@@ -35,27 +35,52 @@ const char *host = HOST;
 const char *port = PORT;
 const char *url = URL;
 
-SoftwareSerial ss(A4, A3);
+SoftwareSerial ss(5, 6);
 GpsReader gpsReader(&ss);
 WifiClientEsp32 wifiClient(wifiSsid, wifiPassword);
 HttpClientEsp32 httpClient(&wifiClient, host, url, port);
 
-GpsCar gpsCar(&gpsReader, &httpClient, &simpleOled);
+// GpsCar gpsCar(&gpsReader, &httpClient, &simpleOled);
 
 void setup()
 {
   Serial.begin(115200);
   ss.begin(9600);
+  wifiClient.begin();
+  // simpleOled.begin();
+  // httpClient.begin();
+  // if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+  // {
+  //   Serial.println(F("SSD1306 allocation failed"));
+  // }
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
-  {
-    Serial.println(F("SSD1306 allocation failed"));
-  }
-
-  gpsCar.begin();
+  // gpsCar.begin();
 }
 
 void loop()
 {
-  gpsCar.loop();
+  // display.clearDisplay();
+  // display.setTextSize(1); // Draw 2X-scale text
+  // display.setTextColor(SSD1306_WHITE);
+  // display.setCursor(0, 0);
+  // display.println(F("initializing..."));
+  // display.display();
+  gpsReader.readGpsData();
+  if (gpsReader.isReady()){
+    Serial.println("Date/time:");
+    Serial.println(gpsReader.getGpsDateTime());
+    Serial.println("Coordinates:");
+    Serial.println(gpsReader.getGpsLatLng());
+  } else {
+    Serial.println("No gps connection");
+  }
+  if(wifiClient.isConnected()){
+    Serial.println("Local IP:");
+    Serial.println(wifiClient.getLocalIP());
+    // simpleOled.writeFirstRow(wifiClient.getLocalIP());
+  } else {
+    Serial.println('No connection to wifi');
+  }
+  delay(1000);
+  // gpsCar.loop();
 }
--- libraries/GpsReader/GpsReader.cpp
+++ libraries/GpsReader/GpsReader.cpp
@@ -3,68 +3,83 @@
 #include <SoftwareSerial.h>
 #include <GpsData.h>
 
-GpsReader::GpsReader(SoftwareSerial *ss) {
-    this->ss = ss;
-    this->gpsReadDelay = DEFAULT_GPS_READ_DELAY;
+GpsReader::GpsReader(SoftwareSerial *ss)
+{
+  this->ss = ss;
+  this->gpsReadDelay = DEFAULT_GPS_READ_DELAY;
 }
 
-void GpsReader::readGpsData() {
+void GpsReader::readGpsData()
+{
   unsigned long start = millis();
-  do 
+  do
   {
-    while (ss->available()) {
+    while (ss->available())
+    {
       gps.encode(ss->read());
     }
   } while (millis() - start < this->gpsReadDelay);
-    if (this->gps.date.isValid() && this->gps.time.isValid()) {
-        this->gpsData.month = this->gps.date.month();
-        this->gpsData.day = this->gps.date.day();
-        this->gpsData.year = this->gps.date.year();
-        this->gpsData.hour = this->gps.time.hour();
-        this->gpsData.minute = this->gps.time.minute();
-        this->gpsData.second = this->gps.time.second();
-        this->gpsData.isDateTimeReady = true;
-    }
-    if (this->gps.location.isValid()) {
-        this->gpsData.latitude = this->gps.location.lat();
-        this->gpsData.longitude = this->gps.location.lng();
-        this->gpsData.isLocationReady = true;
-    }
-    this->fixTimeByGmt();
+  if (this->gps.date.isValid() && this->gps.time.isValid())
+  {
+    this->gpsData.month = this->gps.date.month();
+    this->gpsData.day = this->gps.date.day();
+    this->gpsData.year = this->gps.date.year();
+    this->gpsData.hour = this->gps.time.hour();
+    this->gpsData.minute = this->gps.time.minute();
+    this->gpsData.second = this->gps.time.second();
+    this->gpsData.isDateTimeReady = true;
+  }
+  if (this->gps.location.isValid())
+  {
+    this->gpsData.latitude = this->gps.location.lat();
+    this->gpsData.longitude = this->gps.location.lng();
+    this->gpsData.isLocationReady = true;
+  }
+  this->fixTimeByGmt();
 }
 
-char* GpsReader::getGpsData() {
-    return this->gpsData.getGpsData();
+char *GpsReader::getGpsData()
+{
+  return this->gpsData.getGpsData();
 }
 
-char* GpsReader::getGpsDateTime() {
+char *GpsReader::getGpsDateTime()
+{
   return this->gpsData.getGpsDateTime();
 }
 
-char* GpsReader::getGpsLatLng() {
+char *GpsReader::getGpsLatLng()
+{
   return this->gpsData.getGpsLatLng();
 }
 
-bool GpsReader::isReady() {
-    // Serial.println("Date time is ready:");
-    // Serial.println(this->gpsData.isDateTimeReady);
-    // Serial.println("Location is ready:");
-    // Serial.println(this->gpsData.isLocationReady);
-    return this->gpsData.isDateTimeReady && this->gpsData.isLocationReady;
+bool GpsReader::isReady()
+{
+  // Serial.println("Date time is ready:");
+  // Serial.println(this->gpsData.isDateTimeReady);
+  // Serial.println("Location is ready:");
+  // Serial.println(this->gpsData.isLocationReady);
+  return this->gpsData.isDateTimeReady && this->gpsData.isLocationReady;
 }
 
-void GpsReader::fixTimeByGmt() {
-  if (this->isReady()) {
+void GpsReader::fixTimeByGmt()
+{
+  if (this->isReady())
+  {
     this->GMT = this->gpsData.latitude / 12;
     // Winter time
-    if (this->gpsData.month > 10 || this->gpsData.month < 5) {
+    if (this->gpsData.month > 10 || this->gpsData.month < 5)
+    {
       this->GMT = this->GMT - 1;
     }
     // Fix next day
-    if (this->gpsData.hour + GMT >= 24) {
+    if (this->gpsData.hour + GMT >= 24)
+    {
       this->gpsData.hour = this->gpsData.hour + this->GMT - 24;
-      this->gpsData.day = this->gpsData.day + 1; 
-    } else {
+      this->gpsData.day = this->gpsData.day + 1;
+    }
+    else
+    {
       this->gpsData.hour = this->gpsData.hour + this->GMT;
     }
   }
